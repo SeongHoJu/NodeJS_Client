e51c78030cd676032459c3e05ce8709ca519496d
SeongHoJu <hakgu87@gmail.com>
2015-11-27 13:18:27 +0900

    [PROG_ADD] Node.js Client
    WPF C# Client Using WebSocket

---
+++ NodeJS_Client/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using WebSocketSharp;
+using System.Timers;
+using System.Collections.Concurrent;
+
+
+
+
+
+namespace NodeJS_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public WebConnect WebConnector;
+        string ConnectAddress;
+
+        public UserInfo User;
+        public bool IsLogin = false;
+
+        public void LoginSuccess( UserInfo ServerUserInfo )
+        {
+            User = ServerUserInfo;
+            IsLogin = true;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            WebConnector = new WebConnect();
+
+            //ConnectAddress = "ws://localhost:8080";
+            ConnectAddress = "ws://192.168.102.116:8080";
+
+            WebConnector.ConnectWebSocket(ConnectAddress);
+            WebConnector.appWin = this;
+        }
+
+        public void SendString_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsLogin == false)
+                return;
+
+            JSonPacket ChatPacket = new JSonPacket();
+            ChatPacket.Protocol = "request_chat";
+
+            String ChattingText = ChatInputBox.Text;
+            ChatPacket.AddPacket(User.UserID);
+            ChatPacket.AddPacket(ChattingText);
+            
+            WebConnector.SendPacket(ChatPacket);
+            ChatInputBox.Clear();
+        }
+
+        public void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string InputUserID = Input_ID.Text;
+            string InputPassword = Input_Password.Password;
+
+            JSonPacket LoginPacket = new JSonPacket();
+            LoginPacket.Protocol = "request_login";
+            
+            LoginPacket.AddPacket(InputUserID);
+            LoginPacket.AddPacket(InputPassword);
+
+            WebConnector.SendPacket(LoginPacket);
+        }
+
+        public delegate void LogDelegate(string arg);
+        public void SetLogText(string log)
+        {
+            ServerLog.Dispatcher.BeginInvoke(
+                System.Windows.Threading.DispatcherPriority.Normal,
+                new LogDelegate(UpdateLogText), log);
+        }
+
+        public void UpdateLogText(string log)
+        {
+            ServerLog.Items.Add(log);
+        }
+
+    }
+
+    public struct UserInfo
+    {
+        public string UserID;
+        public string UserServerIndex;
+    };
+}
---
+++ NodeJS_Client/WebConnect.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WebSocketSharp;
+
+namespace NodeJS_Client
+{
+    public class WebConnect
+    {
+        public WebSocket Socket;
+        public MainWindow appWin;
+
+        public WebConnect()
+        { }
+
+        public void ConnectWebSocket(string Address)
+        {
+            Socket = new WebSocket(Address, "connection");
+            Socket.OnMessage += ServerToClientOnMessage;
+            Socket.OnClose += ServerToClientDisconnected;
+
+            Socket.Connect();
+        }
+
+        public async void ServerToClientOnMessage(object sender, MessageEventArgs e)
+        {
+            Task<JSonPacket> PacketTask = JsonConvert.DeserializeObjectAsync<JSonPacket>(e.Data);
+            JSonPacket ReceivePacket = await PacketTask;
+
+            if (ReceivePacket.Protocol == "request_login")
+                request_login_OK(ReceivePacket);
+            else if (ReceivePacket.Protocol == "request_chat")
+                request_chat_OK(ReceivePacket);
+        }
+
+
+        public void ServerToClientDisconnected(object sender, CloseEventArgs e)
+        {
+            
+        }
+
+        public void SendPacket(JSonPacket Packet)
+        {
+            Socket.Send(JsonConvert.SerializeObject(Packet));
+        }
+
+        public void request_login_OK(JSonPacket LoginPacket)
+        {
+            string UserID = LoginPacket.Packets[0].ToString();
+            string UserNumber = LoginPacket.Packets[1].ToString();
+
+            UserInfo ServerUserInfo = new UserInfo();
+            ServerUserInfo.UserID = UserID;
+            ServerUserInfo.UserServerIndex = UserNumber;
+
+            appWin.LoginSuccess(ServerUserInfo);
+            appWin.SetLogText(UserID + "  " + UserNumber + " Connected");
+        }
+
+        public void request_chat_OK(JSonPacket ChatPacket)
+        {
+            string ChatUser = ChatPacket.Packets[0].ToString();
+            string ChatData = ChatPacket.Packets[1].ToString();
+
+            appWin.SetLogText(ChatUser + ": " + ChatData);
+        }
+    }
+
+    public struct JSonPacket
+    {
+        public string Protocol { get; set; }
+        public List<Object> Packets { get; set; }
+
+        public void AddPacket( Object value )
+        {
+            if (Packets == null)
+                Packets = new List<Object>();
+
+            Packets.Add(value);
+        }
+    };
+
+}
